3999b640100860a9f24039be2ca0b08f0d44ef91
Alberto <alberto.izquierdomo@gmail.com>
2020-11-20 20:39:22 +0100

    Frontend. Add input and output file

--- AlbertoIzq-5.PhotoPyDesktopApp/frontend.py
+++ AlbertoIzq-5.PhotoPyDesktopApp/frontend.py
@@ -1,5 +1,14 @@
 from tkinter import *
 
+CATEGORY = [
+"",
+"Restaurant",
+"Bar",
+"Supermarket",
+"Transport",
+"Other"
+]
+
 class Window(object):
     """Class to store window object"""
 
@@ -7,7 +16,47 @@ class Window(object):
         self.window = window
         self.window.title("PhotoPy by Alberto Izquierdo")
 
-        # buttons, labels and shite
+        # Input file
+        l1 = Label(window, text = "Input file")
+        l1.grid(row = 0, column = 0, columnspan = 2)
+
+        l2 = Label(window, text = "Path")
+        l2.grid(row = 1, column = 0)
+
+        l3 = Label(window, text = "Full file name")
+        l3.grid(row = 2, column = 0)
+
+        self.input_path = StringVar()
+        self.e1 = Entry(window, textvariable = self.input_path, width = 60)
+        self.e1.grid(row = 1, column = 1)
+
+        self.input_file_name = StringVar()
+        self.e2 = Entry(window, textvariable = self.input_file_name, width = 60)
+        self.e2.grid(row = 2, column = 1)
+
+        b1 = Button(window, text = "Load")
+        b1.grid(row = 2, column = 2)
+
+        # Output file
+        l4 = Label(window, text = "Output file")
+        l4.grid(row = 3, column = 0, columnspan = 2)
+
+        l5 = Label(window, text = "Path")
+        l5.grid(row = 4, column = 0)
+
+        l6 = Label(window, text = "Full file name")
+        l6.grid(row = 5, column = 0)
+
+        self.output_path = StringVar()
+        self.e3 = Entry(window, textvariable = self.output_path, width = 60)
+        self.e3.grid(row = 4, column = 1)
+
+        self.output_file_name = StringVar()
+        self.e4 = Entry(window, textvariable = self.output_file_name, width = 60)
+        self.e4.grid(row = 5, column = 1)
+
+        b2 = Button(window, text = "Save")
+        b2.grid(row = 5, column = 2)
 
 window = Tk()
 Window(window)
